a5cf5f094d89c5fd65a3edc9c826fb0b8fc75a11
allan raymond <raymonda916@gmail.com>
2025-10-22 21:26:31 +0300

    ipc handling

---
+++ electron/main/ipc-handlers.ts
@@ -0,0 +1,63 @@
+// Sending clipboard data from Electron's main process to the React app.
+
+import { BrowserWindow, ipcMain, IpcMain,  } from "electron";
+import { getAllClips,getClipsByCategory,updateClipCategory,deleteClip,searchClips } from "./database/clips";
+import { getAllCategories, insertCategory, updateCategory, deleteCategory } from './database/categories'
+
+export function setupIpcHandlers() {
+
+    // clips
+    ipcMain.handle('clips:getAll', async(_event, limit?:number) => {
+        return getAllClips(limit)
+    })
+
+    ipcMain.handle('clips:getByCategory', async(_event, categoryId:string) => {
+        return getClipsByCategory(categoryId)
+    })
+
+    ipcMain.handle('clips:delete', async (_event, clipId: string) => {
+    deleteClip(clipId)
+    return {success: true}
+    
+  })
+
+  ipcMain.handle('clips:update', async (_event, clipId: string, categoryId:string | null) => {
+    updateClipCategory(clipId,categoryId)
+    return { success: true }
+  })
+
+  ipcMain.handle("clips:search", async(_event, query: string) => {
+    return searchClips(query)
+  })
+
+    // Categories
+  ipcMain.handle('categories:getAll', async () => {
+    return getAllCategories()
+  })
+
+  
+  ipcMain.handle('categories:create', async (_event, name: string, color: string, icon?: string) => {
+    return insertCategory(name, color, icon)
+  })
+
+
+  ipcMain.handle('categories:update', async (_event, id: string, updates: any) => {
+    updateCategory(id, updates)
+    return { success: true }
+  })
+
+  
+  ipcMain.handle('categories:delete', async (_event, id: string) => {
+    deleteCategory(id)
+    return { success: true }
+  })
+
+  console.log('IPC handlers registered')
+}
+
+//sending new clip to all windows
+export function notifyClip(clip:any): void {
+    BrowserWindow.getAllWindows().forEach(window => {
+        window.webContents.send("clip:send",clip)
+    })
+}
