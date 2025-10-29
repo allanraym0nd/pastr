3e16196e95672a150ecc6278f4e0c6c9a5d46ad9
allan raymond <raymonda916@gmail.com>
2025-10-29 13:08:53 +0300

    updated preload script

--- electron/preload/index.ts
+++ electron/preload/index.ts
@@ -3,16 +3,18 @@ import { getClipsByCategory, updateClipCategory } from "../main/database/clips";
 
 const { contextBridge, ipcRenderer } = require('electron');;
 
-// Expose protected methods that allow the renderer process to use the ipcRenderer without exposing the entire object
+// Job: Expose a safe, limited API to React
+// Uses ipcRenderer to send messages to the main process
 
 contextBridge.exposeInMainWorld('electron', {
 
  clips: {
     getAll: (limit?: number) => ipcRenderer.invoke('clips:getAll', limit),
-   getByCategory: (categoryId: string) => ipcRenderer.invoke('clips:getByCategory', categoryId),
+    getByCategory: (categoryId: string) => ipcRenderer.invoke('clips:getByCategory', categoryId),
     delete: (clipId: string) => ipcRenderer.invoke('clips:delete', clipId),
     updateClipCategory: (clipId: string, categoryId: string) => ipcRenderer.invoke("clips:updateClipCategory", clipId,categoryId),
     search: (query: string) => ipcRenderer.invoke('clips:search', query),
+    getImage: (imagePath: string) => ipcRenderer.invoke('clips:getImage', imagePath), 
     //on new lets react listen to new clipboard items
     onNew: (callback: (clip:any) => void) => { 
         const subscription = (_event:any, clip:any) => callback(clip)
